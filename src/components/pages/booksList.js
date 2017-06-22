9bd21c769a51bbef763e9532208cd32a62b51187
stecrv <stefano.caravana@gmail.com>
2017-06-22 16:21:50 +0100

    style with bootstrap

--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {getBooks} from '../../actions/booksActions';
-
+import {Grid, Col, Row, Button} from  'react-bootstrap';
 class BooksList extends React.Component{
     componentDidMount(){
       this.props.getBooks()
@@ -14,16 +14,21 @@ class BooksList extends React.Component{
             this.props.books.map(function(booksArr){
                 return(
                     <div key={booksArr.id}>
-                    <h2>{booksArr.title}</h2>
-                    <h2>{booksArr.description}</h2>
-                    <h2>{booksArr.price}</h2>
+                        <h2>{booksArr.title}</h2>
+                        <h2>{booksArr.description}</h2>
+                        <h2>{booksArr.price}</h2>
+                        <Button bsStyle='primary' >Buy</Button>
                     </div>
                 )
             });
         return(
             <div>
                 <h1>List of books</h1>
-                {booksList}
+                <Grid style={{marginTop:'15px'}}>
+                    <Row>
+                        {booksList}
+                    </Row>
+                </Grid>
             </div>
         )
     }

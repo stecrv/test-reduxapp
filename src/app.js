7ca1f15d73bd367f9e8fdadfe60fccee3717f846
stecrv <stefano.caravana@gmail.com>
2017-06-22 13:27:58 +0100

    react init

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 "use strict"
+
+import React from 'react';
+import {render} from 'react-dom';
+
 import {applyMiddleware, createStore} from 'redux';
 import logger from 'redux-logger';
 
@@ -9,6 +13,12 @@ import {postBooks, deleteBooks, updateBooks}  from './actions/booksActions';
 //  1 create the store
 const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
+
+import BooksList from './components/pages/booksList';
+
+render(
+  <BooksList />, document.getElementById('app')
+);
 
 store.subscribe(function () {
     console.log('current state is: ',
---
+++ src/components/pages/booksList.js
@@ -0,0 +1,12 @@
+"use strict"
+import React from 'react';
+class BooksList extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>List of books</h1>
+            </div>
+        )
+    }
+}
+export default BooksList;
